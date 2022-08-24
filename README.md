8b188d3ca294b6771f5ac088f49ff783c41bd095
Nick Laptop <45341924+nitnub@users.noreply.github.com>
2022-08-24 00:23:21 -0400

    begin media query

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Werdle - A Wordle Clone 
+A clone of the popular game Wordle. 
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header-container">
-        <div className="header-section">
+        <div className="header-section hamburger">
           <div className="hamburger-nav"></div>
         </div>
         <div className="header-section">
           <div className="title">Werdle!</div>
         </div>
         <div className="header-section">
-          {' '}
+          
           <div className="nav">
             <div className="nav-link">Settings</div>
             <div className="nav-link">Info</div>
