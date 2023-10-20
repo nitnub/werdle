30e23b09dce35fd9a614350262f448b0613b8b99
nitnub <45341924+nitnub@users.noreply.github.com>
2023-10-19 23:03:48 -0700

    update keypresshandler and add laout default

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -1,55 +1,18 @@
 import { useEffect } from 'react';
-import { action } from '../../context/game.actions';
 import { KeyboardService } from './Keyboard.service';
+
 export default function Keyboard({ state, dispatch }) {
-  const keyLayout = [
-    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
-    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-    ['Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Del'],
-  ];
-
-  const allowableKeys = [...Array(26)].map((elem, index) =>
-    String.fromCharCode(index + 97)
-  );
-
-  // const letters = [...Array(26)].map((elem, index) =>
-  //   String.fromCharCode(index + 97)
-  // );
-
-  allowableKeys.push('enter', 'backspace');
-
-  const ks = new KeyboardService(dispatch);
+  const ks = new KeyboardService(state, dispatch);
+  const keyLayout = ks.getLayout();
 
   useEffect(() => {
     const physicalKeyEvent = (e) => {
-      if (state.gameOver) return;
-      const guess = e.key.toLowerCase();
-
-      ks.dispatchKeyEntry(guess);
-      // if (letters.includes(guess))
-      //   dispatch({ type: action.keyEventLetter, payload: guess });
-      // if (guess === 'backspace')
-      //   dispatch({ type: action.keyEventDelete, payload: guess });
-      // if (guess === 'enter') dispatch({ type: action.endTurn, payload: guess });
+      ks.keyPressHandler(e);
     };
 
     window.addEventListener('keydown', physicalKeyEvent);
     return () => window.removeEventListener('keydown', physicalKeyEvent);
   });
-
-  const clickHandler = (e) => {
-    if (state.gameOver) return;
-    // const pressedKey = e.target.innerText.toLowerCase();
-    const guess = e.target.innerText.toLowerCase();
-
-    ks.dispatchKeyEntry(guess);
-    // if (letters.includes(pressedKey))
-    //   dispatch({ type: action.keyEventLetter, payload: pressedKey });
-    // if (pressedKey === 'del')
-    //   dispatch({ type: action.keyEventDelete, payload: pressedKey });
-    // if (pressedKey === 'enter')
-    //   dispatch({ type: action.endTurn, payload: pressedKey });
-  };
 
   return (
     <div data-testid="keyboard" className="keyboard-container">
@@ -63,7 +26,7 @@ export default function Keyboard({ state, dispatch }) {
                   className={`keyboard-key ${key.toLowerCase()} ${
                     state.keyColors[key]
                   }`}
-                  onClick={clickHandler}
+                  onClick={ks.keyClickHandler}
                 >
                   {key}
                 </div>
--- src/components/Keyboard/Keyboard.service.js
+++ src/components/Keyboard/Keyboard.service.js
@@ -2,23 +2,51 @@ import { getRoundIndex } from '../../context/game.helpers';
 import { action } from '../../context/game.actions';
 
 export class KeyboardService {
+  state;
+  dispatch;
+
+  deleteOptions = ['backspace', 'del'];
   letters = [...Array(26)].map((elem, index) =>
     String.fromCharCode(index + 97)
   );
 
-  dispatch;
-
-  constructor(dispatch) {
+  constructor(state, dispatch) {
+    this.state = state;
     this.dispatch = dispatch;
   }
-  // dispatchKeyEntry = (dispatch, guess) => {
+
+
+  keyPressHandler = (e) => {
+    this.dispatchKeyEntry(e.key);
+  };
+
+  keyClickHandler = (e) => {
+    this.dispatchKeyEntry(e.target.innerText);
+  };
+
   dispatchKeyEntry = (guess) => {
+    guess = guess.toLowerCase();
     if (this.letters.includes(guess))
       this.dispatch({ type: action.keyEventLetter, payload: guess });
-    else if (['backspace', 'del'].includes(guess))
+    else if (this.deleteOptions.includes(guess))
       this.dispatch({ type: action.keyEventDelete, payload: guess });
     else if (guess === 'enter')
       this.dispatch({ type: action.endTurn, payload: guess });
+  };
+
+  getLayout = (style = 'QWERTY') => {
+    const QWERTY = [
+      ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+      ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+      ['Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Del'],
+    ];
+
+    switch (style) {
+      case 'QWERTY':
+        return QWERTY;
+      default:
+        return QWERTY;
+    }
   };
 
   updateKeyColors = (state) => {
@@ -42,22 +70,3 @@ export class KeyboardService {
   };
 }
 
-// export const updateKeyColors = (state) => {
-//   const solutionArray = state.solution.split('');
-//   let tempKeys = {};
-
-//   const roundIndex = getRoundIndex(state);
-
-//   state.board[roundIndex - 1].forEach((guess, index) => {
-//     if (state.keyColors[guess] === 'correct' || tempKeys[guess] === 'correct')
-//       return;
-//     if (guess === solutionArray[index]) {
-//       tempKeys = { ...tempKeys, [guess]: 'correct' };
-//     } else if (state.solution.indexOf(guess) >= 0) {
-//       tempKeys = { ...tempKeys, [guess]: 'close' };
-//     } else {
-//       tempKeys = { ...tempKeys, [guess]: 'incorrect' };
-//     }
-//     return { ...state.keyColors, ...tempKeys };
-//   });
-// };
